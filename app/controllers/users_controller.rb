e895693d01a01bee56fe5c4701a1d5d1339f23e9
yang_a104 <yang_a104@163.com>
2014-02-11 16:10:05 +0800

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ end
 def create
     @user = User.new(user_params)
     if @user.save
+    sign_in @user
     flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,6 +17,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
@@ -101,6 +102,10 @@ describe "with a password that's too short" do
     end
   end
 
+describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 end
 
 
